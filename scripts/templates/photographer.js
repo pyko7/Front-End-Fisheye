845a486a2bcb8b5ec52317cdc815ede67206117f
julien henry <julienhenry967@gmail.com>
2023-10-28 10:33:48 +0200

    feat: add link to card

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -17,13 +17,18 @@ function createCardElement() {
   };
 }
 
+/**
+ * 
+ * @param {Object} data 
+ * @returns 
+ */
 function photographerTemplate(data) {
   const { name, id, city, country, tagline, price, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
-    const article = document.createElement("article");
+    const card = document.createElement("a");
     const { img, h2, h3, tagline, price } = createCardElement();
 
     h2.textContent = data.name;
@@ -35,14 +40,15 @@ function photographerTemplate(data) {
     img.style.objectFit = "cover";
     img.setAttribute("src", picture);
     img.setAttribute("alt", name);
+    card.setAttribute('href',`/?id=${data.id}`)
 
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(h3);
-    article.appendChild(tagline);
-    article.appendChild(price);
+    card.appendChild(img);
+    card.appendChild(h2);
+    card.appendChild(h3);
+    card.appendChild(tagline);
+    card.appendChild(price);
 
-    return article;
+    return card;
   }
   return {
     name,
